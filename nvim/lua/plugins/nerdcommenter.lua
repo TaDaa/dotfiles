1a0fd25af44468796ebca73012aeec434f7dbbb6
TaDaa <trevor002@gmail.com>
2024-12-01 02:14:06 -0500

    Fix nerdcommenter error

--- nvim/lua/plugins/nerdcommenter.lua
+++ nvim/lua/plugins/nerdcommenter.lua
@@ -1,11 +1,3 @@
 return {{
-  'preservim/nerdcommenter',
-  config = function()
-    local opts = {
-      silent = true,
-      noremap = true,
-      expr = true 
-      replace_keycodes = false
-    }
-  end
+  'preservim/nerdcommenter'
 }}
