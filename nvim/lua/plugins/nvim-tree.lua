2d0be73b133ffbdf8d42d3f7ece7f4433d9a629f
TaDaa <trevor002@gmail.com>
2024-12-01 09:27:04 -0500

    Switch to oil.nvim, add icons to telescope

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,11 @@
 return {{
  'nvim-tree/nvim-tree.lua',
+  enabled = false,
   version = "*",
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+  opts = {
+  }
 }}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 return {{
   'nvim-telescope/telescope.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
   keys = {
     {'<Leader>ff', '<cmd>Telescope find_files<cr>' , desc="Telescope find_files"}
   }
