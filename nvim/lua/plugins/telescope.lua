243630c2708024c42b2062de32602a8e62557c62
TaDaa <trevor002@gmail.com>
2024-12-01 23:51:51 -0500

    Remove nvim-tree and multicursors. Multicursors has a lot of issues with dealing with regex chars in text which makes it a no go. Replace nvim-tree with oil and custom preview function.  Add vim-visual-multi

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,8 @@ return {{
     'nvim-tree/nvim-web-devicons',
   },
   keys = {
-    {'<Leader>ff', '<cmd>Telescope find_files<cr>' , desc="Telescope find_files"}
+    {'<Leader>ff', '<cmd>Telescope find_files<cr>' , desc="Telescope find_files"},
+    {'<Leader>fg', '<cmd>Telescope live_grep<cr>' , desc="Telescope live_grep"},
+    {'<Leader>fb', '<cmd>Telescope buffers<cr>' , desc="Telescope buffers"},
   }
 }}
---
+++ nvim/lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,4 @@
+vim.cmd('let g:VM_maps={}')
+vim.cmd('let g:VM_maps["Select All"]="<C-a>"')
+vim.cmd('let g:VM_maps["Visual All"]="<C-a>"')
+return {{ 'mg979/vim-visual-multi' }}
