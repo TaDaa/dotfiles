c36ce6baa9c8238d4bbdd51e9bbcbab6a4e3de97
TaDaa <trevor002@gmail.com>
2024-12-08 20:49:48 -0500

    Nm disable search mode terrible, breaks offscreen search ...what is this

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -3,13 +3,13 @@ return {{
   ---@type Flash.Config
   config = function()
     require('flash').setup({
-       modes = {search = {enabled = true}, char = {enabled = false}}
+       modes = {search = {enabled = false}, char = {enabled = false}}
     })
     require('flash').toggle()
   end,
   -- stylua: ignore
   keys = {
-    { "/", mode = { "n", "v"}, function() require("flash").jump() end, desc = "Flash" },
+    --{ "/", mode = { "n", "v"}, function() require("flash").jump() end, desc = "Flash" },
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
