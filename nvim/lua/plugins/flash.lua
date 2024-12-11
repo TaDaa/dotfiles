2028b678483c469cbf6bfb44bed104ecc497d0a9
TaDaa <trevor002@gmail.com>
2024-12-11 09:36:43 -0500

    Switch to pounce

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,6 +1,6 @@
 return {{
   'folke/flash.nvim',
-  enabled = true,
+  enabled = false,
   ---@type Flash.Config
   config = function()
     require('flash').setup({
---
+++ nvim/lua/plugins/pounce.lua
@@ -0,0 +1,19 @@
+return {{
+opts = {},
+'TaDaa/pounce.nvim',
+enabled = true,
+config = function()
+    require'pounce'.setup{
+      accept_keys = 'JFKDLSAHGNUVRBYTMICEOXWPQZ',
+      accept_best_key = '<enter>',
+      multi_window = true,
+      debug = false,
+    }
+    local map = vim.keymap.set
+    map("n", "s", function() require'pounce'.pounce { } end)
+    map("n", "S", function() require'pounce'.pounce { do_repeat = true } end)
+    map("x", "s", function() require'pounce'.pounce { } end)
+    map("o", "gs", function() require'pounce'.pounce { } end)
+    map("n", "S", function() require'pounce'.pounce { input = {reg="/"} } end)
+end
+}}
